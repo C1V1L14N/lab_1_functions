21b36dad34cd7bbf68704718276eec1b98407479
Kenny <kenny.mckeown@gmail.com>
2020-10-14 14:28:32 +0100

    first commit functions lab

---
+++ src/python_functions_practice.py
@@ -0,0 +1,56 @@
+def return_10():
+    return 10
+
+def add(number_1, number_2):
+    return number_1 + number_2
+
+def subtract(number_1, number_2):
+    return number_1 - number_2
+
+def multiply(number_1, number_2):
+    return number_1 * number_2
+
+def divide(number_1, number_2):
+    return number_1 / number_2
+
+def length_of_string(test_string):
+    # print(len(test_string))
+    return len(test_string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(month_num):
+    if month_num == 1:
+        return "January"
+    elif month_num == 3:
+        return "March"
+    elif month_num == 9:
+        return "September"
+
+def number_to_short_month_name(month_num):
+    if month_num == 1:
+        return "Jan"
+    elif month_num == 4:
+        return "Apr"
+    elif month_num == 10:
+        return "Oct"
+    
+def calculate_volume_cube(side_length):
+    side_length = 
+    return side_length * side_length * side_length
+
+
+    
+    
+    
+    
+
+
+
+ 
+
+    
